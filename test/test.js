1b7bebf941891333700952a3ee5de51b12e0b391
Jesse Montrose <jesse@ninth.org>
2013-11-20 14:55:00 -0800

    Initial set

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var chai = require("chai");
+var sinon = require("sinon");
+var expect = chai.expect;
+var badger = require("../lib/honeybadger_node");
+
+var request = require ('request');
+
+describe('errorPackage', function() {
+  it('should return a JSON-safe object', function() {
+    expect(JSON.stringify(badger.errorPackage({message:'message body', url:'http://foo.bar'}))).to.be.a('string');
+  });
+});
+
+describe('notify', function() {
+  it('should ', function() {
+    var mock = sinon.mock(request);
+    mock.expects("post").once();
+    badger.notify({message:'foo', url:'bar'})
+  });
+});
