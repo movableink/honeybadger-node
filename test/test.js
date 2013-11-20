40a4a908010ad15e8e78e8e2f675f9fce43abf3c
Jesse Montrose <jesse@ninth.org>
2013-11-20 15:47:07 -0800

    move main js file out of lib

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var chai = require("chai");
 var sinon = require("sinon");
 var expect = chai.expect;
-var badger = require("../lib/honeybadger_node");
+var badger = require("../honeybadger_node");
 
 var request = require ('request');
 
