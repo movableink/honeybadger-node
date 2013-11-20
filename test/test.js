2798bf3f39142328b5213d4cf16a3809f0eeb72d
Jesse Montrose <jesse@ninth.org>
2013-11-20 15:50:08 -0800

    rename main js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var chai = require("chai");
 var sinon = require("sinon");
 var expect = chai.expect;
-var badger = require("../honeybadger_node");
+var badger = require("../fake_honeybadger");
 
 var request = require ('request');
 
